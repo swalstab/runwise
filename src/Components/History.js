a55109ac1c2b61f69e0f8dcf6ab1fa1d5a1e1a62
StfnVlds <stefanie.walstab@gmail.com>
2025-03-10 18:15:53 +0100

    add message to history component

--- src/Components/History.js
+++ src/Components/History.js
@@ -1,5 +1,8 @@
 function History() {
-  return <section></section>;
+  return <section className="section">
+    <h2>History</h2>
+    <p>Not implemented</p>
+  </section>;
 }
 
 export default History;
